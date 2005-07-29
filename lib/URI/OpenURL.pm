42a62093020dc5dcbf3c7459191cdd9ed804de44
Tim Brody <tdb01r@ecs.soton.ac.uk>
2005-07-29 11:48:47 +0000

    Import of TIMBRODY/URI-OpenURL-0.4.2 from CPAN.
    gitpan-cpan-distribution: URI-OpenURL
    gitpan-cpan-version:      0.4.2
    gitpan-cpan-path:         TIMBRODY/URI-OpenURL-0.4.2.tar.gz
    gitpan-cpan-author:       TIMBRODY
    gitpan-cpan-maturity:     released

--- lib/URI/OpenURL.pm
+++ lib/URI/OpenURL.pm
@@ -89,7 +89,7 @@ There are currently two formats for ContextObjects defined in the OpenURL Framew
 
 use vars qw( $VERSION );
 
-$VERSION = '0.4.1';
+$VERSION = '0.4.2';
 
 use strict;
 use URI::Escape;
@@ -106,6 +106,13 @@ use vars qw( @ISA );
 =item $uri = URI::OpenURL->new([$url])
 
 Create a new URI::OpenURL object and optionally initialize with $url. If $url does not contain a query component (...?key=value) the object will be initialized to a valid contextobject, but without any entities.
+
+If you don't want the context object version and encoding specify url_ver e.g.
+
+	use URI::OpenURL;
+	my $uri = URI::OpenURL->new(
+		'http://myresolver.com/openURL?url_ver=Z39.88-2004'
+	);
 
 =cut
 
@@ -126,6 +133,58 @@ sub _init {
 	) unless $self->query_form();
 	$self;
 }
+
+=pod
+
+=item $uri = URI::OpenURL->new_from_hybrid($uri)
+
+Create a new URI::OpenURL object from a hybrid OpenURL (version 0.1 and/or 1.0 KEVS). Use this to parse a version 0.1 (SFX) style OpenURL.
+
+=cut
+
+sub new_from_hybrid
+{
+	my ($class,$uri) = @_;
+	$class = ref($class) || $class;
+	my $self = $class->new($uri);
+	# If we already have a 1.0 OpenURL just return a canonical 1.0
+	my @KEVS = $self->query_form;
+	my %kevs = @KEVS;
+	my $genre = $kevs{'genre'} || 'article';
+	if( $kevs{url_ver} && $kevs{url_ver} eq 'Z39.88-2004' ) {
+		return $self->canonical();
+	}
+	# Initialize the OpenURL (url_ver etc.)
+	$self->query('');
+	$self = $class->new($self);
+	# Tidy up the 0.1 keys
+	for(my $i = 0; $i < @KEVS;) {
+		if( $KEVS[$i] eq 'sid' ) {
+			$self->referrer->id('info:sid/'.$KEVS[$i+1]);
+			splice(@KEVS,$i,2);
+		} elsif( $KEVS[$i] eq 'id' ) {
+			$self->referent->id($KEVS[$i+1]);
+			splice(@KEVS,$i,2);
+		} elsif( $KEVS[$i] eq 'pid' ) {
+			$self->referent->dat($KEVS[$i+1]);
+			splice(@KEVS,$i,2);
+		} else {
+			$KEVS[$i] = 'jtitle' if $KEVS[$i] eq 'title';
+			$i += 2;
+		}
+	}
+	# Map genre onto a ctx format and add the metadata
+	if( $genre =~ '^article|preprint|proceeding$' ) {
+		$self->referent->journal(@KEVS);
+	} elsif( $genre eq 'bookitem' ) {
+		$self->referent->book(@KEVS);
+	} else {
+		die "Unable to handle version 0.1 genre: $genre";
+	}
+	$self;
+}
+
+=pod
 
 =pod
 
@@ -158,6 +217,59 @@ sub init_timestamps {
 	);
 	wantarray ? @old : ($old[0]||$old[1]);
 }
+
+=pod
+
+=item $uri = $uri->as_hybrid()
+
+Return the OpenURL as a hybrid 0.1/1.0 OpenURL (contains KEVs for both versions). Returns a new URI::OpenURL object.
+
+=cut
+
+sub as_hybrid
+{
+	my $self = shift;
+	my @KEVS = $self->query_form;
+	# Add the referent
+	my @md = $self->referent->metadata();
+	# 'title' has been changed to 'jtitle'
+	for(my $i = 0; $i < @md; $i+=2) {
+		$md[$i] = 'title' if($md[$i] eq 'jtitle');
+	}
+	push @KEVS, @md;
+	# Add the referrer's id
+	if( defined($self->referrer->id) ) {
+		push @KEVS,	sid => $self->referrer->id;
+		$KEVS[$#KEVS] =~ s/^info:(?:sid\/)?//;
+	}
+	# Return a new URI (otherwise we pollute ourselves)
+	my $hybrid = new URI::OpenURL($self);
+	$hybrid->query_form(@KEVS);
+	$hybrid;
+}
+
+=item $uri = $uri->canonical()
+
+Return a canonical OpenURL by removing anything that isn't part of the version 1.0 specification.
+
+=cut
+
+sub canonical
+{
+	my $uri = shift->SUPER::canonical();
+	my @KEVS = $uri->query_form();
+	for(my $i = 0; $i < @KEVS; ) {
+		if( $KEVS[$i] !~ /^ctx_ver|ctx_enc|ctx_id|ctx_tim|url_ver|url_tim|url_ctx_fmt|(?:(?:rft|rfe|svc|req|res|rfr)[_\.].+)$/ ) {
+			splice(@KEVS,$i,2);
+		} else {
+			$i += 2;
+		}
+	}
+	$uri->query_form(@KEVS);
+	$uri;
+}
+
+=cut
 
 =pod
 
@@ -287,11 +399,13 @@ Return the respective descriptor using a method interface. An entity may contain
 
 =head1 CHANGES
 
-0.4.1
-	- Timestamps no longer included in default initialization
-	- Added method "init_timestamps" to add timestamps
-0.4
-	- Initial release
+	0.4.2
+		- Added methods for parsing/writing hybrid OpenURLs
+	0.4.1
+		- Timestamps no longer included in default initialization
+		- Added method "init_timestamps" to add timestamps
+	0.4
+		- Initial release
 
 =head1 COPYRIGHT
 
@@ -338,7 +452,8 @@ sub _addattr {
 	my @KEVS = $self->query_form();
 	my $entity = _entity($self);
 	for( my $i = 0; $i < @pairs; $i+=2 ) {
-		push @KEVS, $entity.'_'.$pairs[$i], ($pairs[$i+1]||'');
+		next unless defined($pairs[$i+1]) && length($pairs[$i+1]);
+		push @KEVS, $entity.'_'.$pairs[$i], $pairs[$i+1];
 	}
 	$self->query_form(@KEVS);
 	$self;
@@ -350,8 +465,8 @@ sub _addkevs {
 	my $entity = _entity($self);
 	push @KEVS, $entity.'_val_fmt', $val_fmt if $val_fmt;
 	for( my $i = 0; $i < @pairs; $i+=2 ) {
-		#	if( $pairs[$i] =~ /^id|val_fmt|ref_fmt|ref|dat$/ ) {
-		push @KEVS, $entity.'.'.$pairs[$i], ($pairs[$i+1]||'');
+		next unless defined($pairs[$i+1]) && length($pairs[$i+1]);
+		push @KEVS, $entity.'.'.$pairs[$i], $pairs[$i+1];
 	}
 	$self->query_form(@KEVS);
 	bless $self, 'URI::OpenURL'; # Should catch some broken user code
@@ -398,7 +513,8 @@ sub descriptors {
 }
 
 sub descriptor {
-	my ($self,$key) = @_;
+	my ($self,$key) = splice(@_,0,2);
+	$self->_addattr($key => $_) for @_;
 	my @KEVS = $self->query_form();
 	my @VALS;
 	my $entity = $self->_entity();
@@ -408,11 +524,11 @@ sub descriptor {
 	wantarray ? @VALS : $VALS[0];
 }
 
-sub dat { shift->descriptor('dat') }
-sub id { shift->descriptor('id') }
-sub ref { shift->descriptor('ref') }
-sub ref_fmt { shift->descriptor('ref_fmt') }
-sub val_fmt { shift->descriptor('val_fmt') }
+sub dat { shift->descriptor('dat',@_) }
+sub id { shift->descriptor('id',@_) }
+sub ref { shift->descriptor('ref',@_) }
+sub ref_fmt { shift->descriptor('ref_fmt',@_) }
+sub val_fmt { shift->descriptor('val_fmt',@_) }
 
 # By-value metadata (things with '.' in)
 sub metadata {
